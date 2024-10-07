b2cea0b8440426b1168912c706c32d9edc140ace
Abdul Mohiz <100228651+abmohiz01@users.noreply.github.com>
2024-10-07 11:12:19 +0500

    Increased time with increasing process also, Logging all system calls of the processes

---
+++ task1/task_1.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+
+from bcc import BPF
+from time import sleep
+import sys
+import os
+
+# Check if the name argument is provided
+if len(sys.argv) != 2:
+    print("Usage: sudo python3 helloworld.py <your_name>")
+    exit(1)
+
+name = sys.argv[1]
+
+# Defining BPF program
+program = """
+int hello(void *ctx) {
+    char name[16];
+    bpf_get_current_comm(&name, sizeof(name));
+    bpf_trace_printk("Running process with name: %s\\n", name);
+    return 0;
+}
+"""
+
+b = BPF(text=program)
+clone = b.get_syscall_fnname("clone")
+b.attach_kprobe(event=clone, fn_name="hello")
+b.trace_print
+
+# Print header
+print("%-18s %-16s %-6s %s" % ("TIME(s)", "COMM", "PID", "MESSAGE"))
+
+# Compile and run the C program
+c_program = """
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <your_name>\\n", argv[0]);
+        return 1;
+    }
+    char *name = argv[1];
+    printf("Running process with name: %s\\n", name);
+    // Simulate some activity by calling a system call in a loop
+    for (int i = 0; i < 8; i++) {
+        printf("Process %s is doing some work... (%d)\\n", name, i + 1);
+        sleep(3); // Simulating work with sleep
+    }
+    printf("Process %s completed its work.\\n", name);
+    return 0;
+}
+"""
+
+with open("helloworld.c", "w") as f:
+    f.write(c_program)
+
+os.system(f"gcc -o helloworld helloworld.c")
+os.system(f"./helloworld {name}")
+
+# Trace output
+while True:
+    try:
+        (task, pid, cpu, flags, ts, msg) = b.trace_fields()
+    except ValueError:
+        continue
+    except KeyboardInterrupt:
+        exit()
+    print(b"%-18.9f %-16s %-6d %s" % (ts, task, pid, msg))
